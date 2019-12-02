1311a411895bd378a1d853bed898d0d319661849
Misha <mkaravaev@gmail.com>
2019-12-02 18:12:43 +0500

    add: ETS storage

--- lib/exchange/storage.ex
+++ lib/exchange/storage.ex
@@ -7,12 +7,13 @@ defmodule Exchange.Storage do
   @callback insert(t(), key, payload) :: :ok | {:error, any()}
   @callback update(t(), key, payload) :: :ok | {:error, any()}
   @callback delete(t(), key) :: :ok | {:error, any()}
-  @callback get_from(t(), key) :: [map()] | {:error, any()}
+  @callback get(t(), key) :: map() | [] | {:error, any()}
 
   defstruct [
     :ask_storage,
     :bid_storage,
-    :serializer
+    :serializer,
+    :module
   ]
 
 end
--- lib/exchange/storage/ets.ex
+++ lib/exchange/storage/ets.ex
@@ -4,13 +4,13 @@ defmodule Exchange.Storage.ETS do
   alias Exchange.Storage
   alias Exchange.DefaultSerializer
 
-
   @impl true
   def init(opts \\ [ask_name: :exchange_ask, bid_name: :exchange_bid]) do
     %Storage{
       ask_storage: do_init_table(name: opts[:ask_name]),
       bid_storage: do_init_table(name: opts[:bid_name]),
-      serializer: DefaultSerializer
+      serializer: DefaultSerializer,
+      module: __MODULE__
     }
   end
 
@@ -21,7 +21,7 @@ defmodule Exchange.Storage.ETS do
 
   @impl true
   def update(table_name, key, payload) do
-    :ets.update(table_name, {key, payload})
+    :ets.insert(table_name, {key, payload})
   end
 
   @impl true
@@ -30,12 +30,47 @@ defmodule Exchange.Storage.ETS do
   end
 
   @impl true
-  def get_from(table_name, key) do
-    #TODO
+  def get(table_name, key) do
+    case :ets.lookup(table_name, key) do
+      [] -> nil
+      [data] -> data
+    end
+  end
+
+  def delete_with_shift(table_name, index, _) do
+    delete(table_name, index)
+    from = get(table_name, index + 1)
+    do_shift(table_name, from)
+  end
+
+  defp do_shift(_table_name, nil), do: :ok
+  defp do_shift(table_name, record) do
+    shift_up(table_name, record)
+    next = next(table_name, record)
+    do_shift(table_name, next)
+  end
+
+  def first(table_name) do
+    key = :ets.first(table_name)
+    get(table_name, key)
+  end
+
+  def shift_up(table_name, {index, data}) do
+    delete(table_name, index)
+    insert(table_name, index - 1, data)
+    get(table_name, index + 1)
+  end
+
+  def next(_table_name, nil), do: nil
+  def next(table_name, {index, _record}) do
+    case :ets.next(table_name, index) do
+      "$end_of_table" -> :stop
+      key -> get(table_name, key)
+    end
   end
 
   defp do_init_table(name: name) do
-    :ets.new(name, [:named_table, :protected])
+    :ets.new(name, [:ordered_set, :named_table, :protected])
   end
 
 end
